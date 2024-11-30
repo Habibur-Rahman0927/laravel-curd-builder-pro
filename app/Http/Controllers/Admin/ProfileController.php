d4d4e6d7930af250b50eb2b99c4f2a913430f263
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2024-12-01 01:21:01 +0600

    profile building done and logo setup done

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -10,5 +12,21 @@ class ProfileController extends Controller
     public function index(Request $request)
     {
         return view('admin.profile.profile');
+    }
+
+     /**
+     * Update the user's profile information.
+     */
+    public function update(ProfileUpdateRequest $request): RedirectResponse
+    {
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return redirect()->route('profile')->with('success', 'Profile updated successfully.');;
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -8,9 +8,15 @@ use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\RoleHasPermissionController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\LanguageController;
+use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Auth\PasswordController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin-dashboard');
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
 Route::resource('user', UserController::class);
 Route::get('user-list', [UserController::class, 'getDatatables'])->name('user-list');
